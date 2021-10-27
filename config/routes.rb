58199b1638d444cc524f580c78db042dee482153
gjetost1 <rosen659@umn.edu>
2021-10-27 16:17:31 -0500

    f

--- config/routes.rb
+++ config/routes.rb
@@ -642,12 +642,7 @@ Rails.application.routes.draw do
   # Example resource route with options:
   #   resources :products do
   #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
   #
-  #     collection do
-  #       get 'sold'
   #     end
   #   end
 
