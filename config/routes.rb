fbcff139c477f5b193bbdf3fcb424d34abb04759
gjetost1 <rosen659@umn.edu>
2021-11-04 09:16:38 -0500

    g

--- config/routes.rb
+++ config/routes.rb
@@ -1091,6 +1091,27 @@ Rails.application.routes.draw do
   #     end
   #   end
 
+    #
+    # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+    # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #
+  #     end
+  #   end
+
   #
 
 
